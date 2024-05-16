c2bbd13e2133dcdb996a8afff2ab52f2ea49e363
Fahim <fahim.pust.cse@gmail.com>
2024-05-16 10:35:57 +0600

    more web element selected

--- cypress/e2e/pageObjectModel.cy.js
+++ cypress/e2e/pageObjectModel.cy.js
@@ -14,5 +14,16 @@ describe('login Test', () => {
     //   cy.get("input[name='username']").type('Admin');
     //   cy.get("input[name='password']").type('admin123');
     //   cy.get('.oxd-button').click();
+
+    //cy.get("[class=oxd-text oxd-text--span oxd-main-menu-item--name]").click()
+    cy.contains("PIM").click()
+    cy.get("button[class='oxd-button oxd-button--medium oxd-button--secondary']").click()
+    cy.get("input[placeholder='First name']").type("A")
+    cy.get("input[placeholder='Middle name']").type("B")
+    cy.get("input[placeholder='Last Name']").type("C")
+    cy.get("div[class='oxd-input-group oxd-input-field-bottom-space'] div input[class='oxd-input oxd-input--active']").type("123")
+    cy.get("button[type='submit']").click()
+
+
     });
   })
